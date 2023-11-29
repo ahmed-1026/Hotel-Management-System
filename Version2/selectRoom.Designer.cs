1845afcf1f84974db08ad94570d67b367644b225
HussnainProg <husnain6908087@gmail.com>
2023-11-30 00:00:44 +0500

    User can reserve his room

--- Version2/selectRoom.Designer.cs
+++ Version2/selectRoom.Designer.cs
@@ -30,7 +30,14 @@ namespace Version2
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.checkInDTPicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkOutDTPicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOccupants = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,38 +49,119 @@ namespace Version2
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(674, 169);
+            this.dataGridView1.Size = new System.Drawing.Size(857, 185);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // button1
+            // btnShow
             // 
-            this.button1.Location = new System.Drawing.Point(656, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 51);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnShow.Location = new System.Drawing.Point(822, 483);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 51);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkInDTPicker
+            // 
+            this.checkInDTPicker.Location = new System.Drawing.Point(297, 296);
+            this.checkInDTPicker.Name = "checkInDTPicker";
+            this.checkInDTPicker.Size = new System.Drawing.Size(286, 26);
+            this.checkInDTPicker.TabIndex = 2;
+            this.checkInDTPicker.Value = new System.DateTime(2023, 11, 29, 22, 22, 35, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.label1.Location = new System.Drawing.Point(72, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Check In Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calisto MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.label2.Location = new System.Drawing.Point(56, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Check Out Date:\r\n";
+            // 
+            // checkOutDTPicker
+            // 
+            this.checkOutDTPicker.Location = new System.Drawing.Point(297, 352);
+            this.checkOutDTPicker.Name = "checkOutDTPicker";
+            this.checkOutDTPicker.Size = new System.Drawing.Size(286, 26);
+            this.checkOutDTPicker.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calisto MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.label3.Location = new System.Drawing.Point(55, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "No. of Occupants:";
+            // 
+            // txtOccupants
+            // 
+            this.txtOccupants.Location = new System.Drawing.Point(297, 401);
+            this.txtOccupants.Multiline = true;
+            this.txtOccupants.Name = "txtOccupants";
+            this.txtOccupants.Size = new System.Drawing.Size(286, 36);
+            this.txtOccupants.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(508, 483);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 51);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // selectRoom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtOccupants);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkOutDTPicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkInDTPicker);
+            this.Controls.Add(this.btnShow);
             this.Controls.Add(this.dataGridView1);
             this.Name = "selectRoom";
-            this.Size = new System.Drawing.Size(809, 488);
+            this.Size = new System.Drawing.Size(1027, 619);
             this.Load += new System.EventHandler(this.selectRoom_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DateTimePicker checkInDTPicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker checkOutDTPicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOccupants;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
--- Version2/selectRoom.cs
+++ Version2/selectRoom.cs
@@ -14,6 +14,9 @@ namespace Version2
 {
     public partial class selectRoom : UserControl
     {
+        int id_of_room = 0;
+        int id_of_hotel = 0;
+        int id_of_customer  = 0;
         public selectRoom()
         {
             InitializeComponent();
@@ -55,6 +58,7 @@ namespace Version2
                 try
                 {
                     cmd.ExecuteNonQuery();
+                    MessageBox.Show(hotelid.ToString());
                     //messagebox.show()
                 }
                 catch (Exception c)
@@ -65,7 +69,16 @@ namespace Version2
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 dataGridView1.DataSource = dt;
-                //dataGridView1.ReadOnly = true;
+                // Here i am taking Customer id
+                var con1 = Configuration.getInstance().getConnection();
+                SqlCommand cmd1 = new SqlCommand("select MAX(c.CustomerID) from Customers AS c", con1);
+                object result = cmd1.ExecuteScalar();
+                //MessageBox.Show(Convert.ToInt32(result).ToString());
+                id_of_customer = Convert.ToInt32(result);
+
+               
+
+
 
 
             }
@@ -73,8 +86,7 @@ namespace Version2
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int id_of_room = 0;
-            int id_of_hotel = 0;
+            
             if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
             {
                 DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
@@ -82,6 +94,35 @@ namespace Version2
                 id_of_hotel = Convert.ToInt32(selectedRow.Cells["HotelID"].Value);
                 MessageBox.Show(id_of_room.ToString() + "   " + id_of_hotel.ToString());
             }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            checkInDTPicker.Checked = false;
+            checkOutDTPicker.Checked = false;
+            bool check = false;
+            var con2 = Configuration.getInstance().getConnection();
+            SqlCommand cmd2 = new SqlCommand("Insert into Reservations values(@CustomerID,@HotelID,@CheckInDate,@CheckOutDate,@NumberOfOccupants,@RoomID)", con2);
+            cmd2.Parameters.AddWithValue("@CustomerID", id_of_customer);
+            cmd2.Parameters.AddWithValue("@HotelID", id_of_hotel);
+            cmd2.Parameters.AddWithValue("@CheckInDate", checkInDTPicker.Value);
+            cmd2.Parameters.AddWithValue("@CheckOutDate", checkOutDTPicker.Value);
+            cmd2.Parameters.AddWithValue("@NumberOfOccupants", txtOccupants.Text);
+            cmd2.Parameters.AddWithValue("@RoomID", id_of_room);
+            try
+            {
+                cmd2.ExecuteNonQuery();
+                check = true;
+            }
+            catch (Exception c)
+            {
+                MessageBox.Show(c.Message);
+            }
+            if (check)
+            {
+                MessageBox.Show("Room Selected");
+            }
+
         }
     }
 }
